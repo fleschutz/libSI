5442c89c0224ec958f53c7852b20411ac54e5bee
Markus Fleschutz <markus.fleschutz@atcsim.de>
2025-12-16 16:07:37 +0100

    Updated Mercury.hpp

--- SI/Mercury.hpp
+++ SI/Mercury.hpp
@@ -5,10 +5,17 @@
 
 namespace si
 {
-	namespace Mercury
+	namespace Mercury // source: https://en.wikipedia.org/wiki/Mercury_(planet)
 	{
-CONST( gravity,            3.7, meters_per_second2); // surface gravity
-CONST( human_population,     0, quantity); // as of 2025
+CONST( mean_radius,                  2'439.7, kilometer);
+CONST( flattening,                       0.0009, quantity);
+CONST( surface_area,                7.48e7, kilometer2);
+CONST( volume,                    6.083e10, kilometer3);
+CONST( mass,                     3.3011e23, kilogram);
+CONST( surface_gravity,                3.7, meters_per_second2);
+CONST( equatorial_rotation_velocity,   3.026, meters_per_second);
+CONST( satellites,                     0, quantity);
+CONST( human_population,               0, quantity); // as of 2025
 	}
 }
 
