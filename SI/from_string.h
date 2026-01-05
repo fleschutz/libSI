7f38ba12f721c83ff15fc4eb32da2bbc275a290e
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-01-05 09:53:12 +0100

    Improved SI/from_string.h

--- SI/from_string.h
+++ SI/from_string.h
@@ -2,17 +2,27 @@
 #pragma once
 
 #include <string>
-#include "units.h"
+#include "literals.h"
 
 namespace SI
 {
 	bool from_string(const std::string& str, length& result)
 	{
 		double value;
-		if (std::sscanf(str.c_str(), "%lfm", &value) == 1)
-			result = value * meter(1.0);
+		char unit[1024];
+		if (std::sscanf(str.c_str(), "%lf%s", &value, unit) != 2)
+			return false; // not recognized
+
+		if (unit == "m")
+			result = 1_m * value;
+		else if (unit == "km")
+			result = 1_km * value;
+		else if (unit == "cm")
+			result = 1_cm * value;
+		else if (unit == "mm")
+			result = 1_mm * value;
 		else
-			return false; // string not recognized
+			return false; // unknown unit
 
 		return true;
 	}
@@ -20,10 +30,24 @@ namespace SI
 	bool from_string(const std::string& str, time& result)
 	{
 		double value;
-		if (std::sscanf(str.c_str(), "%lfs", &value) == 1)
-			result = value * second(1.0);
+		char unit[1024];
+		if (std::sscanf(str.c_str(), "%lf%s", &value, unit) != 2)
+			return false; // not recognized
+
+		if (unit == "s")
+			result = 1_sec * value;
+		else if (unit == "sec")
+			result = 1_sec * value;
+		else if (unit == "m")
+			result = 1_min * value;
+		else if (unit == "min")
+			result = 1_min * value;
+		else if (unit == "h")
+			result = 1_h * value;
+		else if (unit == "hrs")
+			result = 1_h * value;
 		else
-			return false; // string not recognized
+			return false; // unknown unit
 
 		return true;
 	}
