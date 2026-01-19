cb1015ce624d62434a10831136a5b292884ca346
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-01-19 19:26:30 +0100

    Added chemical_elements.h

--- datasets/all.h
+++ datasets/all.h
@@ -1,6 +1,7 @@
-// <datasets/all.h> - includes all datasets. NOTE: convenient but slow
+// <datasets/all.h> - includes all datasets. NOTE: convenient but compilation is slow
 #pragma once
 
+#include "chemical_elements.h"
 #include "planets.h"
 #include "satellites.h"
 #include "exoplanets.h"
