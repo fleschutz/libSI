8f1f09f3cf3e2bff1efa143bccbeafad8907fe77
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-02-18 14:36:52 +0100

    cosmetic changes

--- SI/datatypes.h
+++ SI/datatypes.h
@@ -12,41 +12,41 @@ namespace SI
     using _name ## 2 = _name ## _t<detail::vec2<SIdouble>>;                                                         \
     using _name ## 3 = _name ## _t<detail::vec3<SIdouble>>
 
-	// +++ SI BASE DATATYPES +++     Exponents:
-	//                               l  m  t  T  c  s  i  
+	// +++ SI BASE DATATYPES +++ 
+	//                               l  m  t  T  c  s  i (the exponents for length,mass,...)
 	DATATYPE(length,                 1, 0, 0, 0, 0, 0, 0); // in meters
 	DATATYPE(mass,                   0, 1, 0, 0, 0, 0, 0); // in kilograms
 	DATATYPE(time,                   0, 0, 1, 0, 0, 0, 0); // in seconds
-	DATATYPE(temperature,            0, 0, 0, 1, 0, 0, 0); // in Kelvins (thermodynamic temperature)
-	DATATYPE(electric_current,       0, 0, 0, 0, 1, 0, 0); // in Amperes
+	DATATYPE(temperature,            0, 0, 0, 1, 0, 0, 0); // thermodynamic temperature in kelvins
+	DATATYPE(electric_current,       0, 0, 0, 0, 1, 0, 0); // in amperes
 	DATATYPE(amount_of_substance,    0, 0, 0, 0, 0, 1, 0); // in moles
 	DATATYPE(luminous_intensity,     0, 0, 0, 0, 0, 0, 1); // in candelas
 
 	// +++ DERIVED SI DATATYPES +++
-	DATATYPE(per_length,            -1, 0,  0, 0, 0, 0, 0); // per m (reciprocal)
+	DATATYPE(per_length,            -1, 0,  0, 0, 0, 0, 0); // per meter (reciprocal)
 	DATATYPE(area,                   2, 0,  0, 0, 0, 0, 0); // in square meters
 	DATATYPE(per_area,              -2, 0,  0, 0, 0, 0, 0); // per square meter (reciprocal)
 	DATATYPE(volume,                 3, 0,  0, 0, 0, 0, 0); // in cubic meter
 	DATATYPE(frequency,              0, 0, -1, 0, 0, 0, 0); // per second (reciprocal)
 	DATATYPE(velocity,               1, 0, -1, 0, 0, 0, 0); // in meter per second
-	DATATYPE(acceleration,           1, 0, -2, 0, 0, 0, 0); // in m/s²
-	DATATYPE(jerk,                   1, 0, -3, 0, 0, 0, 0); // in m/s³
-	DATATYPE(force,                  1, 1, -2, 0, 0, 0, 0); // in Newton
-	DATATYPE(energy,                 2, 1, -2, 0, 0, 0, 0); // in Newtonmeter
-	DATATYPE(impulse,                1, 1, -1, 0, 0, 0, 0); // in kg*m/s
-	DATATYPE(pressure,              -1, 1, -2, 0, 0, 0, 0); // in Pascals
-	DATATYPE(torque,                 2, 1, -2, 0, 0, 0, 0); // in kg*m²/s²
-	DATATYPE(power,                  2, 1, -3, 0, 0, 0, 0); // in Watts
-	DATATYPE(power_intensity,        0, 1, -3, 0, 0, 0, 0); // in W/m²
-	DATATYPE(density,               -3, 1,  0, 0, 0, 0, 0); // in kg/m³
+	DATATYPE(acceleration,           1, 0, -2, 0, 0, 0, 0); // in meter per square meter
+	DATATYPE(jerk,                   1, 0, -3, 0, 0, 0, 0); // in meter per cubic meter
+	DATATYPE(force,                  1, 1, -2, 0, 0, 0, 0); // in newtons
+	DATATYPE(energy,                 2, 1, -2, 0, 0, 0, 0); // in newtonmeter
+	DATATYPE(impulse,                1, 1, -1, 0, 0, 0, 0); // in kilogram meter per second
+	DATATYPE(torque,                 2, 1, -2, 0, 0, 0, 0); // in kilogram square meter per square second
+	DATATYPE(pressure,              -1, 1, -2, 0, 0, 0, 0); // in pascals
+	DATATYPE(power,                  2, 1, -3, 0, 0, 0, 0); // in watts
+	DATATYPE(power_intensity,        0, 1, -3, 0, 0, 0, 0); // in watts per square meter
+	DATATYPE(density,               -3, 1,  0, 0, 0, 0, 0); // in kilograms per cubic meter
 	DATATYPE(electric_potential,     2, 1, -3, 0,-1, 0, 0); // in volts
-	DATATYPE(electric_charge,        0, 0,  1, 0, 1, 0, 0); // (electric current and time)
-	DATATYPE(mass_per_area,         -2, 1,  0, 0, 0, 0, 0); // in kg/m²)
+	DATATYPE(electric_charge,        0, 0,  1, 0, 1, 0, 0); // in amperes per second
+	DATATYPE(mass_per_area,         -2, 1,  0, 0, 0, 0, 0); // in kilograms per square meter
 	DATATYPE(per_amount_of_substance,0, 0,  0, 0,-1, 0, 0); // per mol (reciprocal)
 	DATATYPE(angular_speed,          0, 0, -1, 0, 1, 0, 0); // in degrees per second
-	DATATYPE(specific_energy,        2, 0, -2, 0, 0, 0, 0); // in J/kg (or energy per mass or massic energy or gravimetric energy density)
-	DATATYPE(energy_per_mol,         2, 1, -2, 0, 0, 1, 0); // in J/mol
-	DATATYPE(volume_per_time_squared,3, 0, -2, 0, 0, 0, 0); // in m³/s²
+	DATATYPE(specific_energy,        2, 0, -2, 0, 0, 0, 0); // in joules per kilogram
+	DATATYPE(energy_per_mol,         2, 1, -2, 0, 0, 1, 0); // in joules per mol
+	DATATYPE(volume_per_time_squared,3, 0, -2, 0, 0, 0, 0); // in cubic meter per square second
 
 #undef DATATYPE
 } // namespace SI
