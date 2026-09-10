5eb9fd4ae24640905979c7819f15ae61622a6749
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-09-10 15:52:14 +0200

    Updated conversions.cpp

--- examples/conversions.cpp
+++ examples/conversions.cpp
@@ -14,8 +14,11 @@ void conversions() {
     double y = distance / 1_m;  // <- y again contains a dimensionless number (no unit)
 
     // Export a string:
-    std::string result = to_string(distance);
+    std::string result = to_string(distance); // <- result gets "42.00m" assigned
+
+    // Export an equivalent as string:
+    std::string equiv = to_equivalent(distance); // <- equiv gets "45.93yd" assigned
 
 
-    // NOTE: The above doesn't work for celsius and fahrenheit due to the offset!
+    // NOTE: The above conversions don't work for celsius and fahrenheit due to the offset!
 }
