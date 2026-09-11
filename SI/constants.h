0c779b4e5104f81c0108a79a4fcfd1ae95e09fcf
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-09-11 10:05:03 +0200

    Updated SI/constants.h

--- SI/constants.h
+++ SI/constants.h
@@ -1,6 +1,8 @@
-/// @file	constants.h
-/// @brief	Defines 77 type-safe constants such as SI::constant::speed_of_light (source: 2022 CODATA)
-/// @details	Contains: 1. Defined Constants, 2. Universal, 3. Electromagnetic, 4. Atomic & Nuclear, 5. Physico-chemical, 6. Physical, 7. Mathematical, 8. Astronomical, 9. Various, 10. References
+/// @file     SI/constants.h
+/// @brief    Defines 77 type-safe constants, e.g. speed_of_light (source: 2022 CODATA).
+/// @details  Categories are: Defined Constants, Universal Constants, Electromagnetic Constants,
+///           Atomic & Nuclear Constants, Physico-chemical Constants, Physical Constants,
+///           Mathematical Constants, Astronomical Constants, Various Constants, and References.
 
 #pragma once
 #include <SI/units.h>
