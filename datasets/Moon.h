6392351a5c2442bc73d61e9c8279f3abeab72c85
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-09-11 19:00:24 +0200

    Updated datasets/Moon.h

--- datasets/Moon.h
+++ datasets/Moon.h
@@ -1,43 +1,30 @@
-// <dataset/Moon.h> - data of the Moons as of 2025
-#pragma once
+/// @file    dataset/Moon.h
+/// @brief   Contains physical data of the Moon, e.g. Moon.mass.
+/// @details Data as of 2026.
 
+#pragma once
 #include <SI/literals.h>
 using namespace SI;
 
 namespace dataset {
 
-typedef struct {
-	const char* name;
-	SI::mass mass;
-	SI::volume volume;
-	SI::length equatorial_radius;
-	SI::length polar_radius;
-	SI::length mean_radius;
-	SI::dimensionless flattening;
-	SI::length equatorial_circumference;
-	SI::area surface_area;
-	SI::acceleration surface_gravity;
-	SI::velocity equatorial_rotation_velocity;
-	SI::time day;
-} Moon_data;
-
-const Moon_data Moon = {
-	"Moon",
-	7.346e22_kg,
-	2.1958e10_km³,
-	1'738.1_km,
-	1'736.0_km,
-	1'737.4_km,
-	0.0012,
-	10'921_km,
-	3.793e7_km²,
-	1.622_m_per_s²,
-   	4.627_m_per_s,
-	708.7 * 60 * 60_s, 
-};
+    const struct {
+        SI::mass      mass =                      7.346e22_kg;
+        SI::volume    volume =                   2.1958e10_km³;
+        length        equatorial_radius =          1'738.1_km;
+        length        polar_radius =               1'736.0_km;
+        length        mean_radius =                1'737.4_km;
+        dimensionless flattening =                  0.0012;
+        length        equatorial_circumference =    10'921_km;
+        area          surface_area =               3.793e7_km²;
+        acceleration  surface_gravity =              1.622_m_per_s²;
+        velocity      equatorial_rotation_velocity = 4.627_m_per_s;
+        SI::time      day =                708.7 * 60 * 60_s;
+    } Moon;
+ 
+    // References
+    // ----------
+    // 1. https://en.wikipedia.org/wiki/Moon
 
 } // namespace dataset
 
-// Sources
-// -------
-// 1. https://en.wikipedia.org/wiki/Moon
