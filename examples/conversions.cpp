7ca7d62e4d3dd8bdb4395584dda2a992bcc22503
Markus Fleschutz <markus.fleschutz@gmail.com>
2026-09-26 14:37:29 +0200

    Updated conversions.cpp

--- examples/conversions.cpp
+++ examples/conversions.cpp
@@ -3,26 +3,32 @@ using namespace SI;
 
 void conversions() {
 
-    // Import as literal:
+    // Import a Literal
+    // ----------------
     length distance = 42_m;
-    SI::time day = 24_h;
-    mass weight = 100_lb;
+    SI::time day    = 24_h;
+    mass weight     = 100_lb;
 
-    // Import a number:
+    // Import a Number
+    // ---------------
     double x = 42;               // <- x now contains a dimensionless number without unit
     distance = x * 1_m;          // <- distance now contains 42m
 
-    // Import a string:
+    // Import a String
+    // ---------------
     bool check = from_string("42m", distance);
 
 
-    // Export a number:
+    // Export a Number
+    // ---------------
     double y = distance / 1_m;  // <- y again contains a dimensionless number (no unit)
 
-    // Export a string:
+    // Export a String
+    // ---------------
     std::string result = to_string(distance); // <- result gets "42.00m" assigned
 
-    // Export an equivalent as string:
+    // Export an Equivalent
+    // --------------------
     std::string equiv = to_equivalent(distance); // <- equiv gets "45.93yd" assigned
 
 
